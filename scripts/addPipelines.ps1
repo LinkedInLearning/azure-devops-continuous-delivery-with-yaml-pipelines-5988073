f267cd60d7671af2e9167f6aa386dadc498fa45f
Mike <mbenko@benko.com>
2025-11-10 14:57:29 -0600

    Initial commit for 03-05e

---
+++ scripts/addPipelines.ps1
@@ -0,0 +1,15 @@
+# Set up Azure DevOps CLI
+az extension add --name azure-devops
+
+az devops configure --defaults organization=https://dev.azure.com/myOrg project=myProject
+
+$repoName = "Learn-YAMLPipelines"
+$branch = "03-05e"
+
+# CI pipeline
+az pipelines create --repository-type tfsgit --name "CI" --repository $repoName --branch $branch --yml-path .ado/ci.yml --skip-first-run
+az pipelines create --repository-type tfsgit --name "CD" --repository $repoName --branch $branch --yml-path .ado/cd.yml --skip-first-run
+az pipelines create --repository-type tfsgit --name "Infra" --repository $repoName --branch $branch --yml-path .ado/infra.yml --skip-first-run
+
+az pipelines create --repository-type tfsgit --name "PR" --repository $repoName --branch $branch --yml-path .ado/pr-create.yml --skip-first-run
+
