dce1ed248b8026bc266097f035a6d6166e75abfe
Mike <mbenko@benko.com>
2025-11-11 20:11:56 -0600

    Add patterns branch

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,4 +1,4 @@
-git remote add origin https://imademo@dev.azure.com/imademo/Learn-YamlPipelines/_git/Learn-YamlPipelines
+git remote add origin https://<org>@dev.azure.com/<org>/Learn-YamlPipelines/_git/Learn-YamlPipelines
 git push -u origin --all
 
 dotnet new webapp -o src/myApp
